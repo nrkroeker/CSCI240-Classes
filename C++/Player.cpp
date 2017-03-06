e63d4bb59144a969ba5cb1d5f74031375ab3e0d2
nrkroeker <nathaliekroeker@gmail.com>
2017-03-06 11:28:54 -0500

    Build class structure

---
+++ C++/Player.cpp
@@ -0,0 +1,22 @@
+#include "Player.h"
+#include <iostream>
+
+Player::Player() {
+  this->shotNum = 0;
+  this->score = "";
+}
+
+Player::~Player() {
+  //deleteeeee the object
+}
+
+void Player::shoot() {
+
+
+}
+
+void Player::onWin() {
+
+
+  
+}
